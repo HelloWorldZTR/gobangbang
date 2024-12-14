1c6fcae9853a5eaf25211944e1e9d014d26063ce
张庭瑞 <2279429006@qq.com>
2024-12-14 12:36:50 +0000

    优化Service Worker缓存路径，移除根路径前缀

--- js/sw.js
+++ js/sw.js
@@ -1,17 +1,16 @@
 const CACHE_NAME = 'v1';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/css/style.css',
-    '/js/ui.js',
-    '/js/game.js',
-    '/js/replay.js',
-    '/includes/bootstrap.min.css',
-    '/includes/bootstrap.min.js',
-    '/includes/jquery.min.js',
-    '/includes/popper.min.js',
-    '/assests/bg.jpg',
-    '/assests/web_bg.png',
+    'index.html',
+    'css/style.css',
+    'js/ui.js',
+    'js/game.js',
+    'js/replay.js',
+    'includes/bootstrap.min.css',
+    'includes/bootstrap.min.js',
+    'includes/jquery.min.js',
+    'includes/popper.min.js',
+    'assests/bg.jpg',
+    'assests/web_bg.png',
 ];
 
 self.addEventListener('install', event => {
